bb60c1134d834b0eb5f09b9a7cd7bf008623df54
Aljaz Erzin <ae8220@student.uni-lj.si>
2016-03-10 15:21:48 +0000

    Konec

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -13,6 +13,16 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if (cas == 0)
+			{
+				alert("Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!");
+				opomnik.innerHTML = "";
+			}
+			else
+			{
+				cas--;
+				casovnik.innerHTML = cas;
+			}
 		}
 	}
 	var izvediPrijavo=function(event)
@@ -22,16 +32,17 @@ window.addEventListener('load', function() {
 		document.querySelector(".pokrivalo").style.display = "none";
 	}
 	document.querySelector("#prijavniGumb").addEventListener('click',izvediPrijavo);
-	setInterval(posodobiOpomnike, 1000);
+	
 	
 	var dodajOpomnik=function(event)
 	{
 		var naziv = document.querySelector("#naziv_opomnika").value;
 		var cas = document.querySelector("#cas_opomnika").value;
-		document.querySelector("#opomniki").innerHTML+="<div class='opomnik'><div class='naziv_opomnika'>"+naziv+"</div><div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>"
+		document.querySelector("#opomniki").innerHTML+="<div class='opomnik senca rob'><div class='naziv_opomnika'>"+naziv+"</div><div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div></div>"
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
 		
 	}
 	document.querySelector("#dodajGumb").addEventListener('click',dodajOpomnik);
+	setInterval(posodobiOpomnike, 1000);
 });
